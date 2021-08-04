2c817ff13df4515461696c2d838112386182daf5
sajidamullani <85106150+sajidamullani@users.noreply.github.com>
2021-08-04 19:36:21 +0530

    Add files via upload

---
+++ springboot01/springboot01/src/test/java/com/mastek/springboot/Springboot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastek.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
